bd73ac0b9e19ab8b2a7e4dd9c18d15314f5d4da1
narasimha32 <42027498+narasimha32@users.noreply.github.com>
2018-08-05 12:30:40 +0530

    Add files via upload

---
+++ import,dir fuction example files 02-08-18.py
@@ -0,0 +1,107 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+#narasimha.ee@gmail.com
+# different exercises on import function 
+import mymath
+print('addition=',mymath.add(20,30))
+print('addition=',)
+
+
+# In[1]:
+
+
+import mymath
+help(mymath)
+
+
+# In[2]:
+
+
+from math import pi
+print('the value of pi is',pi)
+
+
+# In[3]:
+
+
+import math as m
+print('the valu of pi is',m.pi)
+
+
+# In[10]:
+
+
+from math import *
+print('the value of  log10(x)is', log10(20))
+
+
+# In[7]:
+
+
+import math
+help(math)
+
+
+# In[12]:
+
+
+from math import *
+print('the value of  sqrt 10 is', sqrt(9))
+
+
+# In[13]:
+
+
+dir(mymath)
+
+
+# In[15]:
+
+
+mymath.__name__
+
+
+# In[16]:
+
+
+mymath.__file__
+
+
+# In[18]:
+
+
+import sys
+dir(sys)
+
+
+# In[21]:
+
+
+import imp
+dir(imp)
+
+
+# In[22]:
+
+
+dir()
+
+
+# In[24]:
+
+
+a=200
+b=300
+c='hello'
+import mymath
+
+
+# In[25]:
+
+
+dir()
+
